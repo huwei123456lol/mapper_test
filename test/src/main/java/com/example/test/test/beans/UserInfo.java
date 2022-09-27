d20e32587edb832e62159e8b101863f79ef9ad5b
huwei123456lol <2281896839@qq.com>
2022-09-27 17:54:14 +0800

    提交一个Test项目，测试mapper

---
+++ test/src/main/java/com/example/test/test/beans/UserInfo.java
@@ -0,0 +1,15 @@
+package com.example.test.test.beans;
+
+public class UserInfo {
+
+    private Integer id;
+    private String loginName;
+    private String nickName;
+    private String password;
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String headImage;
+    private String userLevel;
+
+}
